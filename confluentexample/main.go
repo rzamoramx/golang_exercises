453c46fa76984e43dd1063e82595aec175a196fb
Rodrigo Zamora <rodrigo.zamora@cargamos.com>
2022-09-29 10:47:12 -0500

    some refactors and fix security issues

--- confluentexample/main.go
+++ confluentexample/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -12,31 +15,30 @@ import (
 )
 
 var (
-	topic      = "topic_0"
-	producerC  *kafka.Producer
-	consumerC  *kafka.Consumer
-	mechanisms = "PLAIN"
-	protocol   = "SASL_SSL"
-	username   = "username"
-	password   = "password"
-	server     = "server:9092"
+	topic     = "topic_0"
+	producerC *kafka.Producer
+	consumerC *kafka.Consumer
+	conf      map[string]string
 )
-
-// https://github.com/confluentinc/examples/blob/7.2.1-post/clients/cloud/go/producer.go
 
 // RecordValue represents the struct of the value in a Kafka message
 type RecordVal RecordValue
 
 type RecordValue struct {
-	Count int
+	Timestamp int64
+	Count     int
+	Foo       string
 }
 
 func main() {
+	// read config
+	conf = readCCloudConfig("confluent.secret")
+
 	// Create Producer instance
 	producerC = instanceProducer()
 	consumerC = instanceConsumer()
 
-	// Go-routine to handle message delivery reports and
+	// Goroutine to handle message delivery reports and
 	// possibly other event types (errors, stats, etc)
 	go func() {
 		for e := range producerC.Events() {
@@ -52,20 +54,22 @@ func main() {
 		}
 	}()
 
+	// Produce messages in goroutine
 	go producer()
 
+	// Get messages
 	consumer()
 }
 
 func instanceProducer() *kafka.Producer {
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
-		"bootstrap.servers": server,
-		"sasl.mechanisms":   mechanisms,
-		"security.protocol": protocol,
-		"sasl.username":     username,
-		"sasl.password":     password})
+		"bootstrap.servers": conf["bootstrap.servers"],
+		"sasl.mechanisms":   conf["sasl.mechanisms"],
+		"security.protocol": conf["security.protocol"],
+		"sasl.username":     conf["sasl.username"],
+		"sasl.password":     conf["sasl.password"]})
 	if err != nil {
-		fmt.Printf("Failed to create producer: %s", err)
+		fmt.Printf("Failed to create producer: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -74,16 +78,15 @@ func instanceProducer() *kafka.Producer {
 
 func instanceConsumer() *kafka.Consumer {
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": server,
-		"sasl.mechanisms":   mechanisms,
-		"security.protocol": protocol,
-		"sasl.username":     username,
-		"sasl.password":     password,
-		"group.id":          "go_example_group_1", // es arbitrario?
-		"auto.offset.reset": "earliest",
-	})
+		"bootstrap.servers": conf["bootstrap.servers"],
+		"sasl.mechanisms":   conf["sasl.mechanisms"],
+		"security.protocol": conf["security.protocol"],
+		"sasl.username":     conf["sasl.username"],
+		"sasl.password":     conf["sasl.password"],
+		"group.id":          "go_example_group_1",
+		"auto.offset.reset": "earliest"})
 	if err != nil {
-		fmt.Printf("Failed to create consumer: %s", err)
+		fmt.Printf("Failed to create consumer: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -91,10 +94,13 @@ func instanceConsumer() *kafka.Consumer {
 }
 
 func producer() {
+	log.Println("START PRODUCING: " + time.Now().String())
 	for n := 0; n < 10; n++ {
 		recordKey := "alice"
 		data := &RecordValue{
-			Count: n}
+			Count:     n,
+			Timestamp: time.Now().UnixNano() / int64(time.Millisecond),
+			Foo:       "Bar"}
 		recordValue, _ := json.Marshal(&data)
 		fmt.Printf("Preparing to produce record: %s\t%s\n", recordKey, recordValue)
 
@@ -105,14 +111,15 @@ func producer() {
 		}, nil)
 
 		if err != nil {
-			fmt.Printf("Error to produce message: %v", err)
+			fmt.Printf("Error to produce message: %v\n", err)
 		}
 	}
 
 	// Wait for all messages to be delivered
-	producerC.Flush(15 * 1000)
+	producerC.Flush(2 * 1000)
 
 	fmt.Printf("10 messages were produced to topic %s!", topic)
+	log.Println("FINISH PERSIST: " + time.Now().String())
 
 	producerC.Close()
 }
@@ -136,9 +143,9 @@ func consumer() {
 			fmt.Printf("Caught signal %v: terminating\n", sig)
 			run = false
 		default:
+			log.Println("START READING: " + time.Now().String())
 			msg, err := consumerC.ReadMessage(100 * time.Millisecond)
 			if err != nil {
-				// Errors are informational and automatically handled by the consumer
 				//fmt.Printf("Failed to read message: %v\n", err)
 				continue
 			}
@@ -153,9 +160,39 @@ func consumer() {
 			count := data.Count
 			totalCount += count
 			fmt.Printf("Consumed record with key %s and value %s, and updated total count to %d\n", recordKey, recordValue, totalCount)
+			log.Println("FISNIH READING: " + time.Now().String())
 		}
 	}
 
 	fmt.Printf("Closing consumer\n")
 	consumerC.Close()
+}
+
+func readCCloudConfig(configFile string) map[string]string {
+	m := make(map[string]string)
+
+	file, err := os.Open(configFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to open file: %s", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if !strings.HasPrefix(line, "#") && len(line) != 0 {
+			kv := strings.Split(line, "=")
+			parameter := strings.TrimSpace(kv[0])
+			value := strings.TrimSpace(kv[1])
+			m[parameter] = value
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Failed to read file: %s", err)
+		os.Exit(1)
+	}
+
+	return m
 }
